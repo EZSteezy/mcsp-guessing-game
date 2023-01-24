1d0bc02dd43e44aa1918f67794f19ed207dd2f9f
Steven <sdeanjr000@gmaail.com>
2023-01-24 16:05:39 -0600

    lol

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 let userName;
 let numInput;
+let numGen = randomMath.floor(Math.random() * 10);
+
 
 startGame();
+
 
 function startGame() {
 
 let numInput = prompt('pick a number!');
 const num = parseInt(numInput);
-alert('you picked the number ' + num + '!');
+if (num === numGen) {
+    alert('You were right!');
+} else if (num > numGen) {
+    alert('You guessed too high!');
+} else if (num > numGen) {
+    alert('You guessed too low!')
+}
 }
 
