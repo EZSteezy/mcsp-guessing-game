e66bae58a9de6c12dca34866c2fca71407c0589a
Steven <sdeanjr000@gmaail.com>
2023-01-24 15:37:58 -0600

    commit

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-function init() {
+function startGame() {
 let numInput = prompt('pick a number!');
-const num = parseInt();
-console.log(typeOf(num));
-alert('you picked the number ' + num + '!');
 }
