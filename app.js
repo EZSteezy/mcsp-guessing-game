2973fa6404d435d8d527d02fe2645dd7bee9d957
Steven <sdeanjr000@gmaail.com>
2023-01-24 16:50:05 -0600

    pls

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let numInput;
 let numGen = Math.floor(Math.random() * 10);
-let count = 0
+let count = 0;
+let log = [];
 
 alert('Welcome to what should be a working guessing game!')
 startGame();
@@ -12,16 +13,20 @@ const num = parseInt(numInput);
 if (num === numGen) {
     alert('You were right!');
     count++
+    log.push(num);
     alert('you got the answer in ' + count + ' tries!');
+    alert('here are all of your guesses!' += log);
+
 } else if (num > numGen) {
         alert('You guessed too high!');
         count++
         startGame();
-        
+        log.push(num);
         
 } else if (num < numGen) {
         alert('You guessed too low!')
         count++
         startGame();
+        log.push(num);
 }
 }
