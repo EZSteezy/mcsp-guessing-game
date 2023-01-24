8471762a8629a3cc68f73b8abab73b37a161b941
Steven <sdeanjr000@gmaail.com>
2023-01-24 16:35:59 -0600

    well

--- app.js
+++ app.js
@@ -8,20 +8,19 @@ startGame();
 function startGame() {
 let numInput = prompt('pick a number!');
 const num = parseInt(numInput);
-function numCheck()
-}
 
-
-function numCheck(num) {
+function numCheck() {
 if (num === numGen) {
     alert('You were right!');
 } else if (num > numGen) {
         alert('You guessed too high!');
         prompt('pick a number!');
+        function numCheck()
+        
 } else if (num < numGen) {
         alert('You guessed too low!')
         prompt('pick a number!');
+        function numCheck()
 }
 }
-
-
+}
