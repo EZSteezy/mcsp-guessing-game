0dab95f00df05811e9634d66e5ccf424ae97c522
Steven <sdeanjr000@gmaail.com>
2023-01-24 16:43:14 -0600

    commit

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 let numInput;
 let numGen = Math.floor(Math.random() * 10);
-
+let count = 0
 
 startGame();
-
 
 function startGame() {
 let numInput = prompt('pick a number!');
@@ -11,13 +10,17 @@ const num = parseInt(numInput);
 
 if (num === numGen) {
     alert('You were right!');
+    count++
+    alert('you got the answer in ' + count + 'tries!');
 } else if (num > numGen) {
         alert('You guessed too high!');
+        count++
         startGame();
         
         
 } else if (num < numGen) {
         alert('You guessed too low!')
+        coount++
         startGame();
 }
 }
