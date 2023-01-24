d1060bbb0dab57e5089ebda0d96efc6f0641ecbd
Steven <sdeanjr000@gmaail.com>
2023-01-24 15:55:27 -0600

    lol

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ let numInput;
 startGame();
 
 function startGame() {
+
 let numInput = prompt('pick a number!');
 const num = parseInt();
-alert('you picked the number ' + num + '!');
+alert('you picked the number ' + numInput + '!');
 }
 
