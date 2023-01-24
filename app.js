2c51f64ca1189e23e656b9ee5eb34e40cb877291
Steven <sdeanjr000@gmaail.com>
2023-01-24 17:02:20 -0600

    lol

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let numInput;
 let numGen = Math.floor(Math.random() * 10);
 let count = 0;
+let log = []
 
 alert('Welcome to what should be a working guessing game!')
 startGame();
@@ -8,7 +9,6 @@ startGame();
 function startGame() {
 let numInput = prompt('pick a number!');
 const num = parseInt(numInput);
-let logArr = []
 
 if (num === numGen) {
     alert('You were right!');
